a32418b848061a551dea89cebcb583fe8671f348
Jesse Azubuike <jesseazubuike@Jesses-MacBook-Pro.local>
2019-12-30 11:33:02 -0500

    updated RandomAddMult

--- RandomAdditionMultiplicationTesting/RandomAddMult.py
+++ RandomAdditionMultiplicationTesting/RandomAddMult.py
@@ -25,27 +25,32 @@ file_path = './results' + new_file_name
 
 limit = 5
 
-list1 = [i+1 for i in range(limit)]
-list2 = [i+1 for i in range(limit)]
-
 with open(file_path, 'w') as file:
     # initiates the test
-    random.shuffle(list1)
-    for index1 in range(limit):
-        random.shuffle(list2)
-        for index2 in range(limit):
-            correct_answer = 0
+    operand_pairs = []
 
-            if mode == '+':
-                correct_answer = list1[index1] + list2[index2]
-            elif mode == '*':
-                correct_answer = list1[index1] * list2[index2]
+    for i in range(limit):
+        for j in range(limit):
+            operand_pairs.append((i + 1, j + 1))
 
-            question = '{} {} {}'.format(list1[index1], mode, list2[index2])
-            guess = int(input(question + ' = '))
+    random.shuffle(operand_pairs)
 
-            if guess == correct_answer:
-                print("Correct")
-            else:
-                print("Incorrect. Correct answer was " + str(correct_answer))
-                file.write('For {}\nguessed: {}, answer : {}\n\n'.format(question, guess, correct_answer))
+    print(operand_pairs)
+
+    for current_pair in operand_pairs:
+
+        correct_answer = 0
+
+        if mode == '+':
+            correct_answer = current_pair[0] + current_pair[1]
+        elif mode == '*':
+            correct_answer = current_pair[0] * current_pair[1]
+
+        question = '{} {} {}'.format(current_pair[0], mode, current_pair[1])
+        guess = int(input(question + ' = '))
+
+        if guess == correct_answer:
+            print("Correct")
+        else:
+            print("Incorrect. Correct answer was " + str(correct_answer))
+            file.write('For {}\nguessed: {}, answer : {}\n\n'.format(question, guess, correct_answer))
